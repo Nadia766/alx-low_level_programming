0742fa1bb6eb98bde7de7567dee8779d50263e09
Nadia766 <nadiarouimi88@gmail.com>
2023-09-20 12:18:25 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,6 @@ char *cap_string(char *str)
 		str[index - 1] == '\n' ||
 		str[index - 1] == '.' ||
 		str[index - 1] == ';' ||
-		str[index - 1] == '.' ||
 		str[index - 1] == '!' ||
 		str[index - 1] == '?' ||
 		str[index - 1] == '"' ||
