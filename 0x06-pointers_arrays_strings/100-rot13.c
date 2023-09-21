7f3b668583859c6645ae09b3be68e8065d09c862
Nadia766 <nadiarouimi88@gmail.com>
2023-09-21 12:47:35 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,9 +10,9 @@ char *rot13(char *s)
 	int i;
 	int j;
 	char data1[] =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] =
-		"NOPQRSTUVWXYZABCDEFGHIKLMnopqrstuvwxyzabcdefghijklm";
+"NOPQRSTUVWXYZABCDEFGHIKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
